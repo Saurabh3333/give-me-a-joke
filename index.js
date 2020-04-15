874fec503933d1b4adc10d4bdc44d1903adb5a18
Joe Villegas <joexvillegas@gmail.com>
2020-04-15 00:11:32 -0700

    Enhanced extraction of joke text

--- index.js
+++ index.js
@@ -41,20 +41,22 @@ exports.getRandomDadJoke = function (joke) {
 }
 
 exports.getRandomJokeOfTheDay = function (category, joke) {
-    let query
+    let query = ''
     if (category)
         query += `?category=${category}`
 
     var options = {
         url: `https://api.jokes.one/jod${query}`,
         headers: {
-            'Accept': 'application/json'
+            'Content-type': 'application/json'
         }
     };
     request(options, function (error, response, body) {
         if (!error && response.statusCode === 200) {
             var dataJSON = JSON.parse(body);
-            joke(dataJSON.contents.joke[0]);
+            joke(dataJSON.contents.jokes[0].joke.text);
+        } else {
+            throw error
         }
     });
 }
